24c1b8626d4c98c6eedf4bae65179413f45e5d6c
hardw3q <sob.dod@gmail.com>
2021-07-23 17:18:10 +0300

    Added BottomNavigation

--- lib/pages/news.dart
+++ lib/pages/news.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myschoolapp/bottom_nav_bar.dart';
 
 class News extends StatefulWidget {
   const News({Key? key}) : super(key: key);
@@ -10,12 +11,13 @@ class News extends StatefulWidget {
 class _NewsState extends State<News> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Text('Кто прочитал тот лох'),
-           Image.network('https://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/25/25cd22f63a5cc718eb06c4490e2fa3be5cb60c8d_full.jpg')],
-      ),
+    return
+      Scaffold(
+        extendBody: true,
+          bottomNavigationBar: NavigationBar(),
+          body: Container(
+
+          ),
     );
   }
 }
